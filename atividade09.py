b5e4c698c33fe3d29b2722f24a6217fa4f6abd8c
eli <eliadadearaujolinhares@gmail.com>
2025-09-25 14:16:53 -0300

    Add files via upload

---
+++ atividade09.py
@@ -0,0 +1,8 @@
+# Atividade 09 – Cálculo de Área
+# Peça ao usuário a largura e a altura de um retângulo.
+# Calcule e mostre a área total
+
+largura = int(input("digite a largura do retângulo: "))
+altura = int(input("digite a altura do retângulo: "))
+area_total = largura * altura
+print(f'a área total desse retângulo é: {area_total}. ')
